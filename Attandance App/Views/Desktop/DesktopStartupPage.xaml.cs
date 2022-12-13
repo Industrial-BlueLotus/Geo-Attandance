cfd8ba773697543e26828dde499bcbc42ce81f4e
dhvihan <danulvihan@gmail.com>
2022-12-13 16:04:28 +0530

    day end

--- Attandance App/Views/Desktop/DesktopStartupPage.xaml.cs
+++ Attandance App/Views/Desktop/DesktopStartupPage.xaml.cs
@@ -5,11 +5,14 @@ public partial class DesktopStartupPage : ContentPage
     public DateTime time1 { get; set; }
     public DateTime time2 { get; set; }
 
+    public Location location { get; set; }
+
     public DesktopStartupPage()
     {
         InitializeComponent();
         time1 = DateTime.Now;
         time2 = DateTime.Now;
+
         BindingContext = this;
     }
 
@@ -22,12 +25,42 @@ public partial class DesktopStartupPage : ContentPage
     {
         time1 = DateTime.Now;
         Console.WriteLine(time1);
+        GetCachedLocation();
     }
 
     private void OnOutClicked(object sender, EventArgs e)
     {
         time2 = DateTime.Now;
         Console.WriteLine(time2);
+    }
+
+    public async Task<string> GetCachedLocation()
+    {
+        try
+        {
+            //Location location = await Geolocation.Default.GetLastKnownLocationAsync();
+
+            if (location != null)
+                return $"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}";
+        }
+        catch (FeatureNotSupportedException fnsEx)
+        {
+            // Handle not supported on device exception
+        }
+        catch (FeatureNotEnabledException fneEx)
+        {
+            // Handle not enabled on device exception
+        }
+        catch (PermissionException pEx)
+        {
+            // Handle permission exception
+        }
+        catch (Exception ex)
+        {
+            // Unable to get location
+        }
+
+        return "None";
     }
 
 }
