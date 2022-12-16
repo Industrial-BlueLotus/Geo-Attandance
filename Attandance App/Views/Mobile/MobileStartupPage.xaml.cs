172780713e2ca7ca91e3bb8f5c6b31e293614ec4
dhvihan <danulvihan@gmail.com>
2022-12-16 13:41:48 +0530

    ewvwert2

--- Attandance App/Views/Mobile/MobileStartupPage.xaml.cs
+++ Attandance App/Views/Mobile/MobileStartupPage.xaml.cs
@@ -1,34 +1,42 @@
-namespace Attandance_App.Views.Mobile;
 
-public partial class MobileStartupPage : ContentPage
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Attandance_App.Views.Mobile.MobileStartup;
+
+public partial class MobileStartupPage : ObservableObject
 {
-    public DateTime time1 { get; set; }
-    public DateTime time2 { get; set; }
+    //public DateTime Time1 { get; set; }
+    //public DateTime Time2 { get; set; }
 
-    public MobileStartupPage()
-    {
-        InitializeComponent();
-        time1 = DateTime.Now;
-        time2 = DateTime.Now;
-        BindingContext = this;
-    }
+
+
+
 
     private void OnBackClicked(object sender, EventArgs e)
     {
         Shell.Current.GoToAsync("//MainPage");
     }
 
-    private void OnInClicked(object sender, EventArgs e)
-    {
-        time1 = DateTime.Now;
-        Console.WriteLine(time1);
-    }
+    //private void OnInClicked(object sender, EventArgs e)
+    //{
+    //    Time1 = DateTime.Now;
+    //    Console.WriteLine(Time1);
+    //}
 
-    private void OnOutClicked(object sender, EventArgs e)
-    {
-        time2 = DateTime.Now;
-        Console.WriteLine(time2);
-    }
+    //private void OnOutClicked(object sender, EventArgs e)
+    //{
+    //    Time2 = DateTime.Now;
+    //    Console.WriteLine(Time2);
+    //}
+
+    //button1.Click += new EventHandler(button1_Click);
+
+    //private void button1_Click(object sender, EventArgs e)
+    //{
+    //    Time = DateTime.Now.ToString("hh:mm:ss tt");
+    //}
+
 
 
 
