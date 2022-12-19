bdc66e653c774cb8acd04675e4f5b0653a40f1e6
dhvihan <danulvihan@gmail.com>
2022-12-19 13:21:15 +0530

    times are workings

--- Attandance App/ViewModels/AttendenceViewModel.cs
+++ Attandance App/ViewModels/AttendenceViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.Devices.Sensors;
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,23 +20,51 @@ namespace Attandance_App.ViewModels
         [ObservableProperty]
         private DateTime time2;
 
+        [ObservableProperty]
+        private TimeSpan time3;
 
         [ObservableProperty]
-        private string latitude;
+        private Location location;
+
+        [ObservableProperty]
+        private string latitude1;
+
+        [ObservableProperty]
+        private double longtitude1;
 
         [RelayCommand]
         public void OnInButtonClick()
         {
-            int c = 0;
-            Latitude = "Tets Done";
             Time1 = DateTime.Now;
+            GetCachedLocation();
         }
 
+        [RelayCommand]
         public void OnOutButtonClick()
         {
-            int d = 0;
-          
             Time2 = DateTime.Now;
+            Time3 = Time2.Subtract(Time1);
+
+        }
+
+        [RelayCommand]
+        public async Task<string> GetCachedLocation()
+        {
+            try
+            {
+                location = await Geolocation.Default.GetLastKnownLocationAsync();
+
+                if (location != null)
+                    return Latitude1 = location.Latitude;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                // Handle not supported on device exception
+            }
+
+
+            return "None";
         }
 
 
--- Attandance App/Views/Desktop/DesktopStartupPage.xaml.cs
+++ Attandance App/Views/Desktop/DesktopStartupPage.xaml.cs
@@ -40,38 +40,6 @@ public partial class DesktopStartupPage : ContentPage
     }
 
 
-    public async Task<string> GetCachedLocation()
-    {
-        try
-        {
-            //Location location = await Geolocation.Default.GetLastKnownLocationAsync();
-
-            if (location != null)
-                return $"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}";
-        }
-        catch (FeatureNotSupportedException fnsEx)
-        {
-            // Handle not supported on device exception
-        }
-        catch (FeatureNotEnabledException fneEx)
-        {
-            // Handle not enabled on device exception
-        }
-        catch (PermissionException pEx)
-        {
-            // Handle permission exception
-        }
-        catch (Exception ex)
-        {
-            // Unable to get location
-        }
-
-        return "None";
-    }
-
-
-
-
 
 
 }
